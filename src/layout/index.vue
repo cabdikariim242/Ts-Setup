00d60601eecdd2a636d49142369ea8cc179d4383
Cabdikariim <cabdikariimmaxamaddaahir@gmail.com>
2025-08-13 15:59:16 +0300

    sidebar and navbar finished

---
+++ src/layout/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <navbar @toggleSidebar="toggleSidebar" :open="open"/>
+
+  <div class="flex  gap-5 justify-between " >
+    <sidebar @toggleSidebar="toggleSidebar" :open="open"/>
+    <router-view
+      class="flex-1  bg-fuchsia-500 mt-3 mx-2"
+    ></router-view>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import Navbar from "./navbar.vue";
+import Sidebar from "./sidebar.vue";
+export default defineComponent({
+  name: "Layout",
+  components: { Navbar, Sidebar },
+
+data() {
+  return {
+    open: false,
+  };
+},
+methods: {
+  toggleSidebar() {
+    this.open = !this.open;
+  }
+}});
+</script>
+
+<style></style>
---
+++ src/layout/navbar.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="bg-[#63bcf4] w-full h-20 flex items-center justify-between">
+    <div class="flex items-center h-full ml-3">
+        <img class="w-10 hidden lg:flex md:flex " src="@/assets/logo.png" alt="">
+        <h1 class="text-white hidden lg:flex md:flex  dark:text-[gray] text-[30px] font-bold ml-2">vue Logo</h1>
+        <i @click="$emit('toggleSidebar')" class="fa-solid fa-bars flex lg:hidden md:hidden text-[#fffdfd] dark:[gray] text-[30px]"></i>
+    </div>
+    <div class="flex items-center gap-5 text-[25px] dark:text-[gray] text-[#fffdfd]">
+    <i class="fa-solid fa-magnifying-glass"></i>
+    <DarkModeToggle />
+    <i class="fa-solid fa-bell"></i>
+    <img src="@/assets/yiksi.png" class="w-14 mr-5 rounded-full  " alt="User Avatar">
+
+    </div>
+</div>  
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import DarkModeToggle from '@/components/DarkModeToggle.vue'
+
+export default defineComponent({
+  components: {
+    DarkModeToggle
+  },
+  props: {
+    open: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
