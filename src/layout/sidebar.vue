b888839cf49fe63cf39294ce272dee5b8628f169
Abdirahmaan12 <abdirahmaanmoha2022@gmail.com>
2025-08-16 11:12:35 +0300

    added new router

--- src/layout/sidebar.vue
+++ src/layout/sidebar.vue
@@ -6,13 +6,61 @@
         :to="{ name: 'forms' }"
         >input group</router-link
       >
+
+
+       <div class="w-full">
+        <router-link
+        :to="{ name: 'Buttons' }"
+        class="flex mx-2 rounded-lg mt-4 hover:shadow-[0_4px_6px_rgba(0,0,0,0.6)] 
+        " @click="showrouter = !showrouter">
+         <div class="ml-5 my-4 gap-3 mx-6 flex justify-start items-center">
+          <i class="fa-brands fa-elementor text-[#4b93b1] text-[20px] dark:text-white"></i>
+          <p
+            v-if="!toggle"
+            class="group-hover:flex text-[15px] hidden text-[#000000] font-[500] dark:text-white"
+          >
+            FORM ELEMENTS
+          </p>
+          <p v-if="toggle" class="text-[#000000] text-[15px] font-[500">
+            FORM ELEMENTS
+          </p>
+        
+        </div>
+          
+         </router-link>
+
+
+        <div v-if="showrouter" class="border-2 m-5 mr-2 border-[#e2e8f0]">
+          <router-link 
+        :to="{ name: 'Buttons' }"
+        exact-active-class="dropdown-active"
+        class="rounded-lg  hover:shadow-[0_4px_6px_rgba(0,0,0,0.6)] "
+      >
+        <div class=" gap-3 m-3 flex justify-start items-center">
+          <i class="fa-solid fa-circle-down text-[#ffffff] text-[18px] dark:text-white"></i>
+          <p
+            v-if="!toggle"
+            class="group-hover:flex text-[15px] hidden text-[#465569] font-[500] link-text dark:text-white "
+          >
+            Buttons
+          </p>
+          <p v-if="toggle" class="text-[#465569] text-[15px] font-[500] link-text dark:text-white ">
+            Buttons
+          </p>
+        </div>
+      </router-link>
+
+     
+       
+        </div>
+      </div>
     
     </div>
   </div>
 
   <!-- Sidebar for phones -->
   <div
-    v-if="open"
+    v-if="isopen"
     class="flex lg:hidden md:hidden bg-gray-300 w-full max-w-[200px] h-screen"
   >
        <div class="flex flex-col gap-2 w-full mt-2 mx-3">
@@ -30,12 +78,18 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  props: {
-    open: {
+   props: ["isopen"],
+  data() {
+    return {
+      toggle: false,
+      hover: false,
+      showrouter: false,
+      hideTimer: null,
       type: Boolean,
       default: false,
-    },
-  },
+    };
+  }
+ 
 });
 </script>
 
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,8 +1,10 @@
 import type { RouteRecordRaw } from 'vue-router'
 import forms from '@/views/forms.vue'
+import Buttons from '@/views/buttons.vue'
 
 const routes: RouteRecordRaw[] = [
   { path: '/forms', name: 'forms', component: forms },
+  { path: '/Buttons', name: 'Buttons', component: Buttons },
 ]
 
 export default routes
